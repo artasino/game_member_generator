763f7d0c6cc1f500aec8c7debb93fe3c26c113f1
kenta <ktaksn11@gmail.com>
2026-04-03 11:45:14 +0900

    refactor(widgets): update match history scoring logic to prioritize larger match sets

--- lib/presentation/widgets/match_history_widgets.dart
+++ lib/presentation/widgets/match_history_widgets.dart
@@ -897,7 +897,7 @@ class _MatchSettingsDialogState extends State<MatchSettingsDialog> {
           double score =
               pow((mg + md * 4 + mix * 2) / m - (fg + wd * 4 + mix * 2) / f, 2)
                   .toDouble();
-          if (score < minScore) {
+          if (score < minScore || (md + wd + mix) > result.length) {
             minScore = score;
             result = [
               ...List.filled(wd, MatchType.womenDoubles),
